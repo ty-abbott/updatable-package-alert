c496fbf70dc5a876411c452bd7be08f81f5d5447
ty-abbott <tkabbott97@gmail.com>
2024-08-25 05:06:46 +0000

    almost done with base

--- main/updatable-package.py
+++ main/updatable-package.py
@@ -1,5 +1,27 @@
 import os
-
+import smtplib
+from email.message import EmailMessage
+#import ssl
 os.system('apt update')
 os.system('apt list --upgradable > upgradable.txt')
+if os.path.getsize('upgradable.txt') != 0:
+    from_address = os.environ.get('bot_email')
+    to_address = os.environ.get('to_email')
+    smtp_server = 'smtp.gmail.com'
+    port = 465
+    auth_token = os.environ.get('bot_password')
+    subject = 'upgradable packages'
+    body = """
+    hey there this is a test """
+    
+    em = EmailMessage()
+    em['From'] = from_address
+    em['To'] = to_address
+    em['Subject'] = subject
+    em.set_content(body)
+     
+   # context = ssl.create_default_context
+    with smtplib.SMTP_SSL(smtp_server, port) as server:
+        server.login(from_address, auth_token)
+        server.sendmail(from_address, to_address, em.as_string())
 
